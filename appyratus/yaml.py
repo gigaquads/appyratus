7fb65984fe45934ad07d39b7e721d2e4b3eb1711
Jeff Doss <notdsk@gmail.com>
2018-01-22 23:42:19 -0500

    master: YAML support
    Signed-off-by: Jeff Doss <notdsk@gmail.com>

---
+++ appyratus/yaml.py
@@ -0,0 +1,20 @@
+import os
+import yaml
+
+
+class Yaml(object):
+    @classmethod
+    def from_file(cls, file_path: str):
+        with open(file_path) as yaml_file:
+            data = yaml.load(yaml_file.read())
+            return data
+
+    @classmethod
+    def to_file(cls, file_path: str, data=None):
+        with open(file_path, 'wb') as yaml_file:
+            data = yaml.dump(data)
+            yaml_file.write(data)
+
+    @classmethod
+    def format_file_name(cls, file_name):
+        return "{}.yml".format(file_name)
