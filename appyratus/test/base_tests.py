90c6f6ff0864f8411a0d878258ae4421784e3d99
Jeff Doss <notdsk@gmail.com>
2018-06-23 11:20:18 -0400

    test-tools: Base tests common functionality for testing
    Signed-off-by: Jeff Doss <notdsk@gmail.com>

---
+++ appyratus/test/base_tests.py
@@ -0,0 +1,65 @@
+from abc import abstractmethod
+from mock import patch, MagicMock
+from contextlib import contextmanager
+import inspect
+
+
+class BaseTests(object):
+    """
+    # Base Tests
+    Provides common test functionality for testing
+    """
+
+    @property
+    @abstractmethod
+    def klass(self):
+        """
+        Class associated to this test
+        """
+        return
+
+    def instance(self, *args, **kwargs):
+        """
+        Build an object from defined class
+        """
+        return self.klass(*args, **kwargs)
+
+    @property
+    def module_path(self):
+        """
+        Path to the target class's module
+        """
+        return inspect.getmodule(self.klass).__name__
+
+    @property
+    def klass_path(self):
+        """
+        Path to the target class
+        """
+        return "{}.{}".format(self.module_path, self.klass.__name__)
+
+    @contextmanager
+    def mock(self, path: str=None, method: str=None, prop: str=None, **kwargs):
+        """
+        # Mock
+        Establish a mock object relative to the target module.
+
+        # Args
+        - `path`: Additional path, relative to the module path
+        - `method`: an optional method to mock on the module class
+        - `prop`: mock a property on a module class
+        - `kwargs`: additional key word args to be passed into the pytest patch method
+        """
+        if not path:
+            path = self.klass.__name__
+        if method:
+            path = "{}.{}".format(path, method)
+        if prop:
+            path = "{}.{}".format(path, prop)
+        path = "{}.{}".format(self.module_path, path)
+        with patch(path, **kwargs) as mock_obj:
+            if prop:
+                mock_obj.__get__ = MagicMock(**kwargs)
+                yield mock_obj
+            else:
+                yield mock_obj
---
+++ appyratus/test/mark.py
@@ -0,0 +1,9 @@
+from pytest import mark
+
+# High-level
+unit = mark.unit
+integration = mark.integration
+performance = mark.performance
+slow = mark.slow
+skip = mark.skip
+params = mark.parametrize
