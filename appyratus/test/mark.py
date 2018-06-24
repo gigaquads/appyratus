24c9612568bada038b03bb37dfc9abb4bfa15cfe
Jeff Doss <notdsk@gmail.com>
2018-06-24 15:40:21 -0400

    test-tools: Some notes and the test init file
    Signed-off-by: Jeff Doss <notdsk@gmail.com>

--- appyratus/test/mark.py
+++ appyratus/test/mark.py
@@ -6,4 +6,9 @@ integration = mark.integration
 performance = mark.performance
 slow = mark.slow
 skip = mark.skip
+# XXX It may seem ubiquitous to shorten parametrize to params,
+# however in the right context you should import this module
+# entirely, referenced as `mark.params`, and not be concerned
+# otherwise.  It is worth mentioning that in it's next incarnation,
+# it will be a better `params` method entirely
 params = mark.parametrize
