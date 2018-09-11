4199e15542f718257d8f3d6d5d9f13dd15d99a08
Jeff Doss <notdsk@gmail.com>
2018-09-11 08:58:21 -0400

    better-gui: Removing deprecrated cli crud
    Signed-off-by: Jeff Doss <notdsk@gmail.com>

--- appyratus/cli/crud.py
+++
@@ -1,9 +0,0 @@
-from .subparser import Subparser
-
-from .arg import Arg
-
-
-class CrudSubparser(Subparser):
-    @staticmethod
-    def subparsers():
-        return [Subparser(name='create', args=[Arg(flags=('-n', 'name'))])]
