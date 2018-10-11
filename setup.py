fb25051f851cade13e2cfcf0dd4790d1706781dd
Jeff Doss <notdsk@gmail.com>
2018-10-11 00:07:51 -0400

    fix-anything: Brett to the rescue
    Signed-off-by: Jeff Doss <notdsk@gmail.com>

--- setup.py
+++ setup.py
@@ -4,13 +4,20 @@
 import os
 from setuptools import setup, find_packages
 
-setup(
-    name='appyratus',
-    version='0b1',
-    description="Not yo momma's python tool",
-    author='Gigaquads',
-    author_email='notdsk@gmail.com',
-    url='https://github.com/gigaquads/appyratus.git',
-    packages=find_packages()
+if __name__ == '__main__':
+    HERE = os.path.abspath(os.path.dirname(__file__))
+
+    with open(os.path.join(HERE, 'requirements.txt')) as f:
+        REQUIREMENTS = [s.strip().replace('-', '_') for s in f.readlines()]
+
+    setup(
+        name='appyratus',
+        version='0b1',
+        description="Not yo momma's python tool",
+        author='Gigaquads',
+        author_email='notdsk@gmail.com',
+        install_requires=REQUIREMENTS,
+        url='https://github.com/gigaquads/appyratus.git',
+        packages=find_packages()
     #classifiers=['python3', 'mit-license'],
-)
+    )
