eed1e7b3c54b2a76462725629ddc83ce4979e1e7
Jeff Doss <notdsk@gmail.com>
2019-11-04 00:14:01 +0000

    v1: Adding escape forward slashes for json dump defaulted to False
    Signed-off-by: Jeff Doss <notdsk@gmail.com>

--- appyratus/files/json.py
+++ appyratus/files/json.py
@@ -7,6 +7,11 @@ from .file import File
 
 
 class Json(File):
+    """
+    # Json File
+    Backed by ultrajson https://github.com/esnme/ultrajson
+    """
+
     @staticmethod
     def extensions():
         return {'json'}
@@ -16,6 +21,7 @@ class Json(File):
         data = super().read(path)
         return cls.load(data)
 
+    @classmethod
     def write(cls, path: Text, data=None, **kwargs):
         file_data = cls.dump(data, **kwargs)
         super().write(path=path, data=file_data, **kwargs)
@@ -25,5 +31,17 @@ class Json(File):
         return ujson.loads(data) if data else None
 
     @classmethod
-    def dump(cls, data, indent: int = 2, sort_keys: bool = True, **kwargs):
-        return ujson.dumps(data, indent=indent, sort_keys=sort_keys)
+    def dump(
+        cls,
+        data,
+        indent: int = 2,
+        sort_keys: bool = True,
+        escape_forward_slashes: bool = False,
+        **kwargs
+    ):
+        return ujson.dumps(
+            data,
+            indent=indent,
+            sort_keys=sort_keys,
+            escape_forward_slashes=escape_forward_slashes
+        )
