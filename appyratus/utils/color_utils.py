b99f32ecfd3c7f02bf2d8136e54967181fde0c91
Jeff Doss <notdsk@gmail.com>
2019-12-09 01:03:36 +0000

    v1: Color util tests and fixings
    Signed-off-by: Jeff Doss <notdsk@gmail.com>

--- appyratus/utils/color_utils.py
+++ appyratus/utils/color_utils.py
@@ -1,15 +1,18 @@
+import math
+import random
 import re
-import webcolors
 # webcolors is apparently dated.
 # only 138 colors listed but says 140 on
 # https://www.w3schools.com/colors/colors_names.asp
 from typing import Text
 
-from colormath.color_objects import sRGBColor, xyYColor
+import webcolors
+
 from colormath.color_conversions import convert_color
-
-
-
+from colormath.color_objects import (
+    sRGBColor,
+    xyYColor,
+)
 
 
 class ColorUtils(object):
@@ -24,7 +27,12 @@ class ColorUtils(object):
     """
 
     @classmethod
-    def closest_color(cls, value):
+    def closest_name(cls, value):
+        """
+        # Closest Color
+        Using the provided color, get the closest web color name
+        """
+        value = cls.detect_rgb(value)
         min_colors = {}
         for key, name in webcolors.css3_hex_to_names.items():
             r_c, g_c, b_c = webcolors.hex_to_rgb(key)
@@ -35,7 +43,7 @@ class ColorUtils(object):
         return min_colors[min(min_colors.keys())]
 
     @classmethod
-    def get_color_name(cls, value):
+    def get_name(cls, value):
         try:
             closest_name = actual_name = webcolors.rgb_to_name(value)
         except ValueError:
@@ -45,32 +53,35 @@ class ColorUtils(object):
 
     @classmethod
     def to_hex(cls, value):
-        return sRGBColor.get_rgb_hex(value)
+        return sRGBColor(10, 0, 0).get_rgb_hex()
 
     @classmethod
     def hex2rgb(cls, value):
-        return sRGBColor.new_from_rgb_hex(value)
+        hex_value = cls.detect_hex(value)
+        return sRGBColor.new_from_rgb_hex(hex_value).get_upscaled_value_tuple()
 
     @classmethod
-    def parse_hex(cls, value: Text):
+    def detect_hex(cls, value: Text, with_hash: bool = None):
         """
         # Parse Hex
         Parse a value for presence of a hex code
         """
-        hex_format = r'^\#?([A-Fa-f0-9]{1,6})$'
+        hex_format = r'^(\#?)([A-Fa-f0-9]{1,6})'
         hex_regex = re.compile(hex_format)
+        value = str(value)
         result = hex_regex.match(value)
         if not result:
             return
-        hex_value = str(result[0])
+        hex_hash, hex_value = result.groups()
         hex_len = len(hex_value)
-        if hex_len == 2:
-            hex_value = hex_value * 3
-        elif hex_len == 3:
-            hex_value = hex_value * 2
-        else:
-            pass
-        return hex_value
+        if with_hash is not None:
+            if not with_hash:
+                hex_hash = ''
+            elif with_hash and not hex_hash:
+                hex_hash = '#'
+        if hex_len != 6:
+            hex_value = (hex_value * math.ceil(6 / hex_len))[0:6]
+        return hex_hash + hex_value
 
     @classmethod
     def name2hex(cls, value: Text):
@@ -85,10 +96,40 @@ class ColorUtils(object):
         return result[1:]    # remove the '#'
 
     @classmethod
+    def detect_rgb(cls, value):
+        rgb = None
+        if isinstance(value, (list, tuple)):
+            if len(value) == 3:
+                rgb = value
+            if len(value) == 2:
+                xy = cls.detect_xy(value)
+                rgb = cls.xy2rgb(xy)
+        elif isinstance(value, str):
+            hex_value = cls.detect_hex(value)
+            if not hex_value:
+                hex_value = cls.name2hex(value)
+            if hex_value:
+                rgb = cls.hex2rgb(hex_value)
+
+        return rgb
+
+    @classmethod
+    def xy2rgb(cls, value):
+        rgb = None
+        if isinstance(value, (list, tuple)):
+            if len(value) == 3:
+                rgb = value
+            elif len(value) == 2:
+                xy = xyYColor(*value, 0)
+                srgb = convert_color(xy, sRGBColor)
+                rgb = srgb.get_upscaled_value_tuple()
+        return rgb
+
+    @classmethod
     def detect_xy(cls, value):
         rgb = None
         xy = None
-        if isinstance(value, list):
+        if isinstance(value, (list, tuple)):
             if len(value) == 3:
                 rgb = sRGBColor(*value)
             elif len(value) == 2:
@@ -96,7 +137,7 @@ class ColorUtils(object):
         elif isinstance(value, str):
             # value string is one of two things here: a hex value, or a color
             # name which will be translated into hex
-            hex_value = cls.parse_hex(value)
+            hex_value = cls.detect_hex(value)
             if not hex_value:
                 hex_value = cls.name2hex(value)
             if hex_value:
@@ -108,8 +149,8 @@ class ColorUtils(object):
         return xy
 
     @classmethod
-    def random_hex(cls, value):
-        return hex(random.randrange(1 << 32))[4:]
+    def random_hex(cls):
+        return cls.detect_hex(hex(random.randrange(1 << 32))[2:])
 
 
 class Color(object):
@@ -121,7 +162,7 @@ class Color(object):
         self._xy = None
 
     @classmethod
-    def detect_color(cls, value):
+    def detect(cls, value):
         """
         Possible formats
         - name, web name, (CornflowerBlue)
@@ -136,3 +177,23 @@ class Color(object):
         if not self._xy:
             self._xy = self.detect_xy(self._value)
         return self._xy
+
+
+class ColorType(object):
+    pass
+
+
+class RgbColor(ColorType):
+    pass
+
+
+class HexColor(ColorType):
+    pass
+
+
+class XyColor(ColorType):
+    pass
+
+
+class WebNameColor(ColorType):
+    pass
