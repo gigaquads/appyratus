9a37e784504ad1cfb786e728b0167886d322297f
Daniel Gabriele <d.gabri3le@gmail.com>
2020-03-27 12:59:07 -0400

    refactored template utils a bit
    Signed-off-by: Daniel Gabriele <d.gabri3le@gmail.com>

--- appyratus/utils/__init__.py
+++ appyratus/utils/__init__.py
@@ -1,9 +1,15 @@
 from .color_utils import ColorUtils
 from .dict_utils import DictUtils, DictObject
-from .path_utils import PathUtils
-from .template_utils import TemplateUtils, TemplateEnvironment, Template, TemplateFilter
 from .string_utils import StringUtils
 from .time_utils import TimeUtils
 from .func_utils import FuncUtils
 from .sys_utils import SysUtils
 from .list_utils import ListUtils
+from .path_utils import PathUtils
+from .template_utils import (
+    Template,
+    TemplateUtils,
+    TemplateFilter,
+    TemplateEnvironment,
+    JinjaTemplateEnvironment,
+)
