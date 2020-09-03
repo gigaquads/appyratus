84038e33fa35d8543513db5eafda7753c030deca
Jeff Doss <notdsk@gmail.com>
2020-09-03 13:21:41 +0000

    xml-v1: Upgrade sys exec to better support capturing stream data
    Signed-off-by: Jeff Doss <notdsk@gmail.com>

--- appyratus/utils/sys_utils.py
+++ appyratus/utils/sys_utils.py
@@ -10,15 +10,21 @@ from IPython.core import ultratb
 class SysUtils(object):
 
     @classmethod
-    def sys_exec(cls, command: Text, capture=None) -> Text:
+    def sys_exec(cls, command: Text, capture=None, merge_streams=False) -> Text:
         """
         Run a command in a subprocess, output goes to STDOUT
         Or run a command and capture it's output
         """
-        if not capture:
-            return subprocess.call(command.split())
+        if capture:
+            if merge_streams:
+                exec_kwargs = {'stdout': subprocess.PIPE, 'stderr': subprocess.STDOUT}
+            else:
+                exec_kwargs = {'capture_output': True}
+            res = subprocess.run(command.split(), **exec_kwargs)
+            value = res.stdout.decode('utf-8').rstrip()
+            return value
         else:
-            return os.popen(command).read().rstrip()
+            return subprocess.call(command.split())
 
     @classmethod
     def safe_main(cls, main_callable, debug_level: int = None) -> object:
