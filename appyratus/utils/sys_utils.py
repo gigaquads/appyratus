2dddb52a351f53d548212b01ffea2478e458449d
Jeff Doss <notdsk@gmail.com>
2020-11-30 18:52:27 +0000

    master: Improving sysexec with shlex split, alternative write command to disk then execute
    Signed-off-by: Jeff Doss <notdsk@gmail.com>

--- appyratus/utils/sys_utils.py
+++ appyratus/utils/sys_utils.py
@@ -1,30 +1,53 @@
 import os
+import shlex
 import subprocess
 import sys
-
 from typing import Text
 
 from IPython.core import ultratb
 
+from appyratus.files import File
+from appyratus.utils import (
+    PathUtils,
+    TimeUtils,
+)
+
 
 class SysUtils(object):
+    sequence = 0
 
     @classmethod
-    def sys_exec(cls, command: Text, capture=None, merge_streams=False) -> Text:
+    def sys_exec(
+        cls,
+        command: Text,
+        capture=None,
+        merge_streams=False,
+        write_command=False
+    ) -> Text:
         """
-        Run a command in a subprocess, output goes to STDOUT
-        Or run a command and capture it's output
+        Run a command in a subprocess output goes to STDOUT
+
+        - capture, capture STDOUT of the command and return
+        - merge_streams, merge the STDERR stream into the STDOUT stream
+        - write_command, write the command to a shell script and call that
+          script instead.  useful when dealing with a command you want to call
+          without parsing it through shlex
         """
+        if write_command:
+            command = cls.write_command(command)
+
+        command_args = shlex.split(command, comments=True)
         if capture:
             if merge_streams:
                 exec_kwargs = {'stdout': subprocess.PIPE, 'stderr': subprocess.STDOUT}
             else:
                 exec_kwargs = {'capture_output': True}
-            res = subprocess.run(command.split(), **exec_kwargs)
+            res = subprocess.run(command_args, **exec_kwargs)
             value = res.stdout.decode('utf-8').rstrip()
             return value
         else:
-            return subprocess.call(command.split())
+            res = subprocess.call(command_args)
+            return res
 
     @classmethod
     def safe_main(cls, main_callable, debug_level: int = None) -> object:
@@ -55,3 +78,14 @@ class SysUtils(object):
         Resolve bin path
         """
         return SysUtils.sys_exec('/usr/bin/env which {}'.format(bin_file), capture=True)
+
+    @classmethod
+    def write_command(cls, command, path: Text = None):
+        path = path if path else '/tmp'
+        timestamp = TimeUtils.utc_timestamp()
+        cls.sequence += 1
+        tmp_file = f'appyratus-sys-exec-{timestamp}-{cls.sequence}.sh'
+        file_path = PathUtils.join(path, tmp_file)
+        File.write(file_path, f"#!/usr/bin/env bash\n{command}")
+        PathUtils.make_executable(file_path, user=True)
+        return file_path
