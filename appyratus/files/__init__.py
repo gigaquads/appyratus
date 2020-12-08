3f10f7d56ba42726dadce597c455617370912531
Jeff Doss <notdsk@gmail.com>
2020-12-08 01:42:01 +0000

    master: Inits forrit
    Signed-off-by: Jeff Doss <notdsk@gmail.com>

--- appyratus/files/__init__.py
+++ appyratus/files/__init__.py
@@ -2,9 +2,12 @@ from .csv import Csv
 from .base import BaseFile
 from .file import File, FileObject
 from .html import Html
+from .markdown import Markdown
+from .css import Css
 from .ini import Ini
 from .json import Json
 from .python import PythonModule
+from .shell import Shell
 from .text import Text
 from .xml import Xml
 from .yaml import Yaml, YamlFileObject
